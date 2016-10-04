f33ff3a49259380dc14cf21acd5267218eb706ec
mikera <mike.r.anderson.13@gmail.com>
2016-10-04 11:52:57 +0800

    Fix warnings
    Conflicts resolved in:
      src/main/java/us/bpsm/edn/parser/DefaultListFactory.java
      src/main/java/us/bpsm/edn/printer/Printers.java

--- src/main/java/us/bpsm/edn/parser/DefaultListFactory.java
+++ src/main/java/us/bpsm/edn/parser/DefaultListFactory.java
@@ -5,12 +5,15 @@ import java.io.Serializable;
 import java.util.*;
 
 final class DefaultListFactory implements CollectionBuilder.Factory {
-    public CollectionBuilder builder() {
+    @Override
+	public CollectionBuilder builder() {
         return new CollectionBuilder() {
             ArrayList<Object> list = new ArrayList<Object>();
-            public void add(Object o) {
+            @Override
+			public void add(Object o) {
                 list.add(o);
             }
+            @Override
             public Object build() {
                 return new DelegatingList(list);
             }
